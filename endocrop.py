25f38f9a73980817f652edd658ea59414d6d12b8
MIZUGUCHI Yasuhiko <mizugy@gmail.com>
2019-06-09 19:58:48 +0900

    refactor

--- endocrop.py
+++ endocrop.py
@@ -1,14 +1,12 @@
 import click
 import os
 from pathlib import Path
-from PIL import Image
-import PIL
 import concurrent.futures
-from PIL import ImageFilter,ImageChops,ImageMath
 import tqdm
-import numpy as np
+#import numpy as np
 import cv2
 
+from PIL import Image
 def cv2pil(image_cv):
     image_cv = cv2.cvtColor(image_cv, cv2.COLOR_BGR2RGB)
     image_pil = Image.fromarray(image_cv)
@@ -64,8 +62,11 @@ def cmd(src, out, overwrite):
 
 def endo_image_crop(src,out):
     i = cv2.imread(str(src))
+    # grayscale
     ip = cv2.cvtColor(i, cv2.COLOR_BGR2GRAY)
+    # gaussian blur to reduce noise
     ip = cv2.GaussianBlur(ip, (5, 5), 0)
+    # filter
     _, ip = cv2.threshold(ip, 10, 255, cv2.THRESH_BINARY)
     
     ct,_ = cv2.findContours(ip, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
@@ -75,41 +76,8 @@ def endo_image_crop(src,out):
     x,y,w,h = cv2.boundingRect(cnt)
 
     r = i[y:y+h, x:x+w]
-    #cv2pil(r).show()
     cv2.imwrite(str(out), r)
 
-    
-    '''    
-    origin = Image.open(src).convert('RGB') #
-
-    h,s,v = origin.convert('HSV').split()
-    s.show()
-
-    r,g,b = origin.split()
-    m = origin.convert('L')
-
-    i = ImageMath.eval('(((r-m)**2+(g-m)**2+(b-m)**2)>100)*255', r=r, g=g, b=b, m=m)
-    i = i.filter(ImageFilter.MinFilter(3))
-    i.show()
-    bbox = i.getbbox()
-    origin.crop(bbox).show()
-
-    v.show()
-    vm = Image.new('L',i.size)
-    
-    w,h = i.size
-    for y in range(h):
-        for x in range(w):
-            r,g,b = i.getpixel((x,y))
-            m = (r+g+b)/3
-            v = (r-m)**2+(g-m)**2+(b-m)**2
-            c = 255 if v > 200 else 0
-            vm.putpixel((x,y),c)
-            
-    vm = vm.filter(ImageFilter.MinFilter(5))
-    bbox = vm.getbbox()
-    i.crop(bbox).save(out)
-'''    
 
     
 def main():
