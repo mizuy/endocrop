aefc01fcd94c477b30202b5048158871b8b772b8
MIZUGUCHI Yasuhiko <mizugy@gmail.com>
2020-10-18 20:03:38 +0900

    add mask

--- endocrop.py
+++ endocrop.py
@@ -1,68 +1,30 @@
+#!/usr/bin/env python3
 import click
-import os
+import os, functools
 from pathlib import Path
 import concurrent.futures
 import tqdm
 #import numpy as np
 import cv2
 
-from PIL import Image
-def cv2pil(image_cv):
-    image_cv = cv2.cvtColor(image_cv, cv2.COLOR_BGR2RGB)
-    image_pil = Image.fromarray(image_cv)
-    image_pil = image_pil.convert('RGB')
+class Mask:
+    def __init__(self, mask_file):
+        self._mask = cv2.imread(str(mask_file))
+        self._shape = self._mask.shape
 
-    return image_pil
+    def mask(self, image):
+        assert image.shape == self._shape
+        return cv2.bitwise_and(image, self._mask)
 
-@click.command()
-@click.argument('src', type=click.Path(exists=True))
-@click.argument('out')
-@click.option('--overwrite', default=False)
-def cmd(src, out, overwrite):
-    if not os.path.isdir(src):
-        raise
+_mask_fuji = Mask(Path(__file__).parent/'mask_fuji.png')
+_mask_cce = Mask(Path(__file__).parent/'mask_cce.png')
 
-    if os.path.isdir(out):
-        pass
-    elif os.path.isfile(out):
-        raise
-    else:
-        os.makedirs(out)
+assert _mask_fuji._shape == (1024, 1280, 3)
+assert _mask_cce._shape == (512, 512, 3)
 
-    args = []
+def conv_endocrop(image):
+    i = image
     
-    for parent, dirs, files in os.walk(src):
-        for d in dirs:
-            p = Path(os.path.join(parent, d))
-            o = Path(out)/p.relative_to(src)
-            os.makedirs(o, exist_ok=True)
-
-            
-        for f in files:
-            p = Path(os.path.join(parent, f))
-            o = Path(out)/p.relative_to(src).with_suffix('.png')
-
-            if not overwrite and o.exists():
-                continue
-
-            if p.suffix.lower() not in ['.jpg','.png']:
-                continue
-
-
-            args.append((p,o))
-
-
-    with concurrent.futures.ProcessPoolExecutor(max_workers=1) as executor:
-        futures = [executor.submit(endo_image_crop, s,o) for s,o in args]
-        for f in tqdm.tqdm(concurrent.futures.as_completed(futures), total=len(futures), unit='image'):
-            pass
-        dfs = [f.result() for f in futures]
-
-        
-
-def endo_image_crop(src,out):
-    #print(src,out)
-    i = cv2.imread(str(src))
     # grayscale
     ip = cv2.cvtColor(i, cv2.COLOR_BGR2GRAY)
     # gaussian blur to reduce noise
@@ -71,19 +33,83 @@ def endo_image_crop(src,out):
     _, ip = cv2.threshold(ip, 10, 255, cv2.THRESH_BINARY)
     
     ct,_ = cv2.findContours(ip, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    #ii = np.copy(i)
 
     cnt = max(ct, key=lambda cnt: cv2.contourArea(cnt))
     x,y,w,h = cv2.boundingRect(cnt)
 
-    r = i[y:y+h, x:x+w]
-    cv2.imwrite(str(out), r)
+    return i[y:y+h, x:x+w]
 
+def convert(src, out, flag_endocrop, flag_maskfuji, flag_maskcce):
+    i = cv2.imread(str(src))
 
+    if flag_maskfuji:
+        i = _mask_fuji.mask(i)
+
+    if flag_maskcce:
+        i = _mask_cce.mask(i)
+
+    if flag_endocrop:
+        i = conv_endocrop(i)
+
+    cv2.imwrite(str(out), i)
     
-def main():
-    cmd()
+def walk_and_convert(src_dir, dst_dir, rename_map, function, overwrite=False, tqdm_unit='unit'):
+    if not os.path.isdir(src_dir):
+        raise
 
+    if os.path.isdir(dst_dir):
+        pass
+    elif os.path.isfile(dst_dir):
+        raise
+    else:
+        os.makedirs(dst_dir)
+
+    args = []
+    
+    for parent, dirs, files in os.walk(src_dir):
+        for d in dirs:
+            p = Path(os.path.join(parent, d))
+            o = Path(dst_dir)/p.relative_to(src_dir)
+            os.makedirs(o, exist_ok=True)
+
+            
+        for f in files:
+            p = Path(os.path.join(parent, f))
+            dst_name = rename_map(p.relative_to(src_dir))
+            
+            if not dst_name:
+                continue
+            
+            o = Path(dst_dir)/dst_name
+
+            if not overwrite and o.exists():
+                continue
+
+            args.append((p,o))
+
+
+    with concurrent.futures.ProcessPoolExecutor(max_workers=6) as executor:
+        futures = [executor.submit(function, s, o) for s,o in args]
+        for f in tqdm.tqdm(concurrent.futures.as_completed(futures), total=len(futures), unit='image'):
+            pass
+        dfs = [f.result() for f in futures]
+
+@click.command()
+@click.argument('src_dir', type=click.Path(exists=True))
+@click.argument('dst_dir')
+@click.option('--overwrite', is_flag=True)
+@click.option('--endocrop/--no-endocrop', default=True)
+@click.option('--maskfuji', is_flag=True)
+@click.option('--maskcce', is_flag=True)
+def command(src_dir, dst_dir, overwrite, endocrop, maskfuji, maskcce):
+    def rename_map(path):
+        if path.suffix.lower() not in ['.jpg','.png']:
+            return None
+        return path.with_suffix('.jpg')
+
+    function = functools.partial(convert, flag_maskfuji=maskfuji, flag_maskcce=maskcce, flag_endocrop=endocrop)
+
+    walk_and_convert(src_dir, dst_dir, rename_map, function, overwrite, 'image')
 
 if __name__ == '__main__':
-    main()
+    command()
